fa9ab619fc8ff10a697cea306ea7a2dec234e558
AUSTINMORARA <user.name>
2024-07-30 20:37:42 +0300

    sub

--- add.py
+++ add.py
@@ -3,8 +3,14 @@
 def add(x, y):
     return x + y
 
+def sub(x, y):
+    return x - y
+
 def main():
     result = add(4, 7)
     print(result)
+
+    res = sub(6, 8)
+    print(res)
 if __name__ == '__main__':
     main()
